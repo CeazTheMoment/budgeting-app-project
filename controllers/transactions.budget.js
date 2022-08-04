a2ea0350fb9923905b8445e5f9a808a6ccf8bde0
Cesar Ortiz <ceazthemoment@aol.com>
2022-08-04 04:37:28 -0400

    fix error

--- controllers/transactions.budget.js
+++ controllers/transactions.budget.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const budget = express.Router();
+const transactions = express.Router();
 const transactionsArray = require("../models/budget.data");
 
-budget.get("/", (req, res) => {
+transactions.get("/", (req, res) => {
   res.json(transactionsArray);
 });
 
-budget.get("/:id", (req, res) => {
+transactions.get("/:id", (req, res) => {
   const { id } = req.params;
   if (transactionsArray[id]) {
     res.send(transactionsArray[id]);
@@ -15,22 +15,22 @@ budget.get("/:id", (req, res) => {
   }
 });
 
-budget.post("/", (req, res) => {
+transactions.post("/", (req, res) => {
   transactionsArray.push(req.body);
   res.json(transactionsArray[transactionsArray.length - 1]);
 });
 
-budget.delete("/:id", (req, res) => {
+transactions.delete("/:id", (req, res) => {
   const { id } = req.params;
   transactionsArray.splice(id, 1);
   res.send(transactionsArray);
 });
 
-budget.put("/:id", (req, res) => {
+transactions.put("/:id", (req, res) => {
   const { id } = req.params;
   const upData = req.body;
   transactionsArray[id] = upData;
   res.send(transactionsArray[id]);
 });
 
-module.exports = budget;
+module.exports = transactions;
